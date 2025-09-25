3a3439a127a5e2dfa8d85376f1d8bf6fcff7f41b
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2025-09-24 22:45:18 -0300

    Update ConversorController.java

--- src/main/java/br/fiap/conversor_temperatura/controller/ConversorController.java
+++ src/main/java/br/fiap/conversor_temperatura/controller/ConversorController.java
@@ -1,7 +1,24 @@
 package br.fiap.conversor_temperatura.controller;
 
+import br.fiap.conversor_temperatura.service.ConversorService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ConversorController {
+    private final ConversorService service;
+
+    public ConversorController(ConversorService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/converter")
+    public String converter(@RequestParam double celsius, Model model){
+        double fahrenheit = service.celsiusParaFarenheit(celsius);
+        model.addAttribute("fahrenheit", fahrenheit);
+        return "index";
+    }
+
 }
